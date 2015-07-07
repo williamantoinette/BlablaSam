c6f6bcac70aba9c6cf157c8de3114f9c682d5536
William- <williamantoinette@gmail.com>
2015-07-07 09:06:53 +0200

    Google places en cours de mise en place

--- app/src/main/java/fr/itescia/blablasam/blablasam/MainActivity.java
+++ app/src/main/java/fr/itescia/blablasam/blablasam/MainActivity.java
@@ -202,7 +202,7 @@ public class MainActivity extends Activity {
             case 2:
                 //fragment = new MapFragment();
 
-                //this.startActivity(new Intent(this, MapFragment.class));
+                this.startActivity(new Intent(this, MapFragment.class));
                 break;
             case 3:
                 //fragment = new CommunityFragment();
@@ -219,8 +219,8 @@ public class MainActivity extends Activity {
         }
 
         if (fragment != null) {
-            //FragmentManager fragmentManager = getFragmentManager();
-            //fragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit();
 
             // update selected item and title, then close the drawer
             mDrawerList.setItemChecked(position, true);
--- app/src/main/java/fr/itescia/blablasam/blablasam/MapFragment.java
+++ app/src/main/java/fr/itescia/blablasam/blablasam/MapFragment.java
@@ -4,17 +4,35 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.AutocompletePredictionBuffer;
+import com.google.android.gms.location.places.GeoDataApi;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by William- on 06/07/2015.
@@ -35,17 +53,55 @@ public class MapFragment extends Activity {
                     .Builder(this)
                     .addApi(Places.GEO_DATA_API)
                     .addApi(Places.PLACE_DETECTION_API)
+
+
                     .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
                         @Override
                         public void onConnected(Bundle bundle) {
+                            System.out.println("Connexion API OK");
+                            System.out.println("Test connexion : " + googlePlace.isConnected());
 
+                            Thread autoComplete = new Thread(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    System.out.println("Test connexion 2 : " + googlePlace.isConnected());
+                                    LatLng latitude = new LatLng(45, 1);
+                                    LatLng longitude = new LatLng(50, 5);
+
+
+                                    PendingResult<AutocompletePredictionBuffer> result = (Places.GeoDataApi.getAutocompletePredictions(googlePlace, "https://maps.googleapis.com/maps/api/place/queryautocomplete/json?key=AIzaSyAbjNu9IQGy_eFaNwnhoHfHjKLV1xJ2Y08&language=fr&input=pizza+near%20par",
+                                            new LatLngBounds(latitude, longitude), null));
+
+
+                                    AutocompletePredictionBuffer autocompletePredictions = result.await(60, TimeUnit.SECONDS);
+                                    try {
+                                        //Thread.sleep(5000);
+                                    } catch (Exception ex) {
+
+                                    }
+                                    System.out.println(autocompletePredictions.getStatus());
+                                    System.out.println(autocompletePredictions.getCount());
+                                    Iterator<AutocompletePrediction> iterator = autocompletePredictions.iterator();
+                                    while (iterator.hasNext()) {
+                                        AutocompletePrediction prediction = iterator.next();
+
+                                        System.out.println(prediction.getDescription());
+
+
+                                    }
+
+
+                                }
+                            });
+
+                            autoComplete.start();
                         }
 
                         @Override
                         public void onConnectionSuspended(int i) {
 
-
-
+                            System.out.println("Connexion API OFF");
                         }
                     })
                     .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
@@ -58,6 +114,7 @@ public class MapFragment extends Activity {
 
 
 
+
             googlePlace.connect();
 
             int PLACE_PICKER_REQUEST = 1;
@@ -65,7 +122,7 @@ public class MapFragment extends Activity {
 
             Context context = getApplicationContext();
             try {
-                startActivityForResult(builder.build(context), PLACE_PICKER_REQUEST);
+                //startActivityForResult(builder.build(context), PLACE_PICKER_REQUEST);
             }
             catch (Exception ex)
             {
